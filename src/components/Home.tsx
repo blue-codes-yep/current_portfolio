3667584ba78ebf82a4c8f7c52df872447dcf02f1
blue-codes-yep <linzblue5@gmail.com>
2023-07-05 21:02:40 -0400

    Animations, and style changes

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -7,23 +7,29 @@ function Home() {
         <div className={styles.home}>
             <Loader />
             <div className={styles.intro}>
-                <h1 className={styles.name}>Blue</h1>
-                <h2 className={styles.title}>Software Engineer</h2>
-                <p className={styles.description}>
-                    I'm a software engineer with the ability to pick up new languages and technologies as needed, and with a focus on fast, responsive, and intuitive code. Forever learning, and moving forward with my passion for technology, as well as the advancement of my skills in all aspects.
-                </p>
-            </div>
-            <div className={styles.skills}>
-                <h3>Software Development Skills:</h3>
-                <p>JavaScript, Python, Flask, Pandas, Numpy, Langchain, LLMs, Node.js, React, Express, RESTful API, PostgreSQL, NLPs, Selenium, Beautiful Soup, Playwright, Material UI, Bootstrap, Flexbox, Heroku, AWS, NGNIX, HTML, CSS</p>
-            </div>
-            <div className={styles.contact}>
-                <h3>Contact:</h3>
-                <p>blue.codes.eng@gmail.om</p>
-            </div>
-            <div className={styles.links}>
-                <h3>Links:</h3>
-                <p><a href="https://bluecodes.dev">bluecodes.dev</a> | <a href="https://github.com/blue-codes-yep">GitHub</a></p>
+                <div className={styles.profile}>
+                    <h1 className={styles.name}>Blue</h1>
+                    <h2 className={styles.title}>Software Engineer</h2>
+                </div>
+                <div className={styles.d_skill}>
+                    <p className={styles.description}>
+                        I'm a software engineer with the ability to pick up new languages and technologies as needed, and with a focus on fast, responsive, and intuitive code. Forever learning, and moving forward with my passion for technology, as well as the advancement of my skills in all aspects.
+                    </p>
+                    <div className={styles.skills}>
+                        <h3>Software Development Skills:</h3>
+                        <p>JavaScript, Python, Flask, Pandas, Numpy, Langchain, LLMs, Node.js, React, Express, RESTful API, PostgreSQL, NLPs, Selenium, Beautiful Soup, Playwright, Material UI, Bootstrap, Flexbox, Heroku, AWS, NGNIX, HTML, CSS</p>
+                    </div>
+                </div>
+                <div className={styles.footer}>
+                    <div className={styles.contact}>
+                        <h3>Contact:</h3>
+                        <p>blue.codes.eng@gmail.om</p>
+                    </div>
+                    <div className={styles.links}>
+                        <h3>Links:</h3>
+                        <p><a href="https://bluecodes.dev">bluecodes.dev</a> | <a href="https://github.com/blue-codes-yep">GitHub</a></p>
+                    </div>
+                </div>
             </div>
         </div>
     );
--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,26 +1,49 @@
-import React from 'react';
 import styles from '../styles/Loader.module.scss';
+import React, { useState, useEffect } from 'react';
 
 function Loader() {
+    const [isClicked, setIsClicked] = useState(false);
+    const [position, setPosition] = useState({ x: 0, y: 0 });
+
+    useEffect(() => {
+        const handleClick = (event: MouseEvent) => {
+            setIsClicked(true);
+            setPosition({ x: event.clientX, y: event.clientY });
+            setTimeout(() => setIsClicked(false), 3000); // Reset after 3 seconds
+        };
+
+        window.addEventListener('click', handleClick);
+
+        // Clean up event listener on component unmount
+        return () => {
+            window.removeEventListener('click', handleClick);
+        };
+    }, []);
+
     return (
-        <div className={styles.animationContainer}>
-            <div className={styles.lightningContainer}>
-                <div className={styles.lightning + ' ' + styles.white}></div>
-                <div className={styles.lightning + ' ' + styles.red}></div>
+        <div className={styles['animation-container']}>
+            <div className={styles['lightning-container']}>
+                <div className={`${styles.lightning} ${styles.white}`}></div>
+                <div className={`${styles.lightning} ${styles.red}`}></div>
             </div>
-            <div className={styles.boomContainer}>
-                <div className={styles.shape + ' ' + styles.circle + ' ' + styles.big + ' ' + styles.white}></div>
-                <div className={styles.shape + ' ' + styles.circle + ' ' + styles.white}></div>
-                <div className={styles.shape + ' ' + styles.triangle + ' ' + styles.big + ' ' + styles.yellow}></div>
-                <div className={styles.shape + ' ' + styles.disc + ' ' + styles.white}></div>
-                <div className={styles.shape + ' ' + styles.triangle + ' ' + styles.blue}></div>
-            </div>
-            <div className={styles.boomContainer + ' ' + styles.second}>
-                <div className={styles.shape + ' ' + styles.circle + ' ' + styles.big + ' ' + styles.white}></div>
-                <div className={styles.shape + ' ' + styles.circle + ' ' + styles.white}></div>
-                <div className={styles.shape + ' ' + styles.disc + ' ' + styles.white}></div>
-                <div className={styles.shape + ' ' + styles.triangle + ' ' + styles.blue}></div>
-            </div>
+            {isClicked && (
+                <div style={{ position: 'absolute', top: position.y, left: position.x }}>
+                    <div className={`${styles['boom-container']} ${styles.first}`}>
+                        <div className={`${styles.shape} ${styles['triangle']}`}></div>
+                        <div className={`${styles.shape} ${styles['triangle']} ${styles.big}`}></div>
+                        <div className={`${styles.shape} ${styles['circle']}`}></div>
+                        <div className={`${styles.shape} ${styles['circle']} ${styles.big}`}></div>
+                        <div className={`${styles.shape} ${styles['disc']}`}></div>
+                    </div>
+                    <div className={`${styles['boom-container']} ${styles.second}`}>
+                        <div className={`${styles.shape} ${styles['triangle']}`}></div>
+                        <div className={`${styles.shape} ${styles['triangle']} ${styles.big}`}></div>
+                        <div className={`${styles.shape} ${styles['circle']}`}></div>
+                        <div className={`${styles.shape} ${styles['circle']} ${styles.big}`}></div>
+                        <div className={`${styles.shape} ${styles['disc']}`}></div>
+                    </div>
+                </div>
+            )}
         </div>
     );
 }
