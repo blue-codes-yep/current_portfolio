9b55557d2edc1128650cf4e0a1a4f6b031710dab
blue-codes-yep <linzblue5@gmail.com>
2023-07-05 21:33:48 -0400

    Using framer motions for some animations

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,38 +1,43 @@
 import React from 'react';
 import styles from '../styles/Home.module.scss';
 import Loader from './Loader';
+import { motion } from 'framer-motion';
 
 function Home() {
     return (
-        <div className={styles.home}>
+        <motion.div className={styles.home}>
             <Loader />
-            <div className={styles.intro}>
-                <div className={styles.profile}>
-                    <h1 className={styles.name}>Blue</h1>
-                    <h2 className={styles.title}>Software Engineer</h2>
-                </div>
-                <div className={styles.d_skill}>
-                    <p className={styles.description}>
+            <motion.div className={styles.intro}>
+                <motion.div className={styles.profile}>
+                    <motion.h1 className={styles.name}>Blue</motion.h1>
+                    <motion.h2 className={styles.title}>Software Engineer</motion.h2>
+                </motion.div>
+                <motion.div className={styles.d_skill}>
+                    <motion.p className={styles.description}>
                         I'm a software engineer with the ability to pick up new languages and technologies as needed, and with a focus on fast, responsive, and intuitive code. Forever learning, and moving forward with my passion for technology, as well as the advancement of my skills in all aspects.
-                    </p>
-                    <div className={styles.skills}>
-                        <h3>Software Development Skills:</h3>
-                        <p>JavaScript, Python, Flask, Pandas, Numpy, Langchain, LLMs, Node.js, React, Express, RESTful API, PostgreSQL, NLPs, Selenium, Beautiful Soup, Playwright, Material UI, Bootstrap, Flexbox, Heroku, AWS, NGNIX, HTML, CSS</p>
-                    </div>
-                </div>
-                <div className={styles.footer}>
-                    <div className={styles.contact}>
-                        <h3>Contact:</h3>
-                        <p>blue.codes.eng@gmail.om</p>
-                    </div>
-                    <div className={styles.links}>
-                        <h3>Links:</h3>
-                        <p><a href="https://bluecodes.dev">bluecodes.dev</a> | <a href="https://github.com/blue-codes-yep">GitHub</a></p>
-                    </div>
-                </div>
-            </div>
-        </div>
+                    </motion.p>
+                    <motion.div className={styles.skills}>
+                        <motion.h3>Software Development Skills:</motion.h3>
+                        <motion.p>JavaScript, Python, Flask, Pandas, Numpy, Langchain, LLMs, Node.js, React, Express, RESTful API, PostgreSQL, NLPs, Selenium, Beautiful Soup, Playwright, Material UI, Bootstrap, Flexbox, Heroku, AWS, NGNIX, HTML, CSS</motion.p>
+                    </motion.div>
+                </motion.div>
+                <motion.div 
+                    className={styles.footer}
+                    initial={{ y: 100 }} // start 100px below
+                    animate={{ y: 0 }} // animate to its original position
+                    transition={{ duration: 1 }} // animate over 1 second
+                >
+                    <motion.div className={styles.contact}>
+                        <motion.p>blue.codes.eng@gmail.com</motion.p>
+                    </motion.div>
+                    <motion.div className={styles.links}>
+                        <motion.p><a href="https://bluecodes.dev">bluecodes.dev</a> | <a href="https://github.com/blue-codes-yep">GitHub</a></motion.p>
+                    </motion.div>
+                </motion.div>
+            </motion.div>
+        </motion.div>
     );
 }
 
 export default Home;
+
