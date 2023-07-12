a2fc093ae7251e00471076f2afadfb25ca5d4f28
blue-codes-yep <linzblue5@gmail.com>
2023-07-12 17:15:29 -0400

    Current refactor of portfolio site

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,85 +1,31 @@
-import React, { startTransition, useRef, useState, Suspense, useEffect } from 'react';
-import { Canvas } from '@react-three/fiber';
-import { OrbitControls } from '@react-three/drei';
-import { motion } from 'framer-motion';
+import React from 'react';
 import styles from '../styles/Home.module.scss';
 import Menu from './Menu';
-import TextTexture from './TextTexture';
+import { Canvas } from '@react-three/fiber';
 import Particles from './Particles';
 
 function Home() {
-    const footerRef = useRef(null);
-    const skillRef = useRef(null);
-
     const handleProjectClick = () => {
         console.log('Project clicked');
     };
 
     return (
-        <motion.div className={styles.home}
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ duration: 5 }}
-        >
+        <div className={styles.home}>
+            <div className={styles.left}>
+                <h1 className={styles.name}>Blue</h1>
+                <h2 className={styles.title}>Software Engineer</h2>
+            </div>
             <Menu onProjectClick={handleProjectClick} />
-            <motion.div className={styles.intro}>
-                <motion.div className={styles.profile}
-                    initial={{ x: 0 }}
-                    animate={{ x: "-330%" }}
-                    transition={{ duration: 1 }}
-                >
-                    <motion.h1 className={styles.name}
-                        initial={{ y: 0, x: 0 }}
-                        animate={{ y: "35%", x: "-94%" }}
-                        transition={{ duration: 5 }}>Blue</motion.h1>
-                    <motion.h2 className={styles.title}
-                        initial={{ y: 0, x: 0 }}
-                        animate={{ y: "-55%", x: "5%" }}
-                        transition={{ duration: 5 }}>Software Engineer</motion.h2>
-                </motion.div>
-                <motion.div className={styles.d_skill}
-                    initial={{ x: 0 }}
-                    animate={{ x: "160%" }}
-                    transition={{ duration: 5 }}
-                >
-                    <motion.p className={styles.description}
-                        initial={{ x: 0 }}
-                        animate={{ x: "50%" }}
-                        transition={{ duration: 5 }}
-                    >
-                        I'm a software engineer with the ability to pick up new languages and technologies as needed, and with a focus on fast, responsive, and intuitive code. Forever learning, and moving forward with my passion for technology, as well as the advancement of my skills in all aspects.
-                    </motion.p>
-                    <motion.div ref={skillRef} className={styles.skills}
-                        initial={{ x: 0 }}
-                        animate={{ y: "45%", x: "50%" }}
-                        transition={{ duration: 5 }}>
-                        <motion.h3>Software Development Skills:</motion.h3>
-                        <motion.p>JavaScript, Python, Flask, Pandas, Numpy, Langchain, LLMs, Node.js, React, Express, RESTful API, PostgreSQL, NLPs, Selenium, Beautiful Soup, Playwright, Material UI, Bootstrap, Flexbox, Heroku, AWS, NGNIX, HTML, CSS</motion.p>
-                    </motion.div>
-                </motion.div>
-                <motion.div ref={footerRef} className={styles.footer}
-                    initial={{ y: 0, x: 0 }}
-                    animate={{ y: "-350%", x: "-44%" }}
-                    transition={{ duration: 5 }}
-                >
-                    <motion.div className={styles.contact}>
-                        <motion.p>blue.codes.eng@gmail.com</motion.p>
-                    </motion.div>
-                    <motion.div className={styles.links}>
-                        <motion.p><a href="https://bluecodes.dev">bluecodes.dev</a> | <a href="https://github.com/blue-codes-yep">GitHub</a></motion.p>
-                    </motion.div>
-                </motion.div>
-            </motion.div>
+            <div className={styles.right}>
+                <p className={styles.description}>
+                    I'm a software engineer with the ability to pick up new languages and technologies as needed, and with a focus on fast, responsive, and intuitive code. Forever learning, and moving forward with my passion for technology, as well as the advancement of my skills in all aspects.
+                </p>
+            </div>
             <Canvas style={{ position: 'fixed', top: 0, left: 0, width: '100%', height: '100%', zIndex: -1 }}>
-                <ambientLight />
                 <Particles count={5000} />
-                <Suspense fallback={null}>
-                    {/* Will maybe revist animating text manually later <TextTexture text="blue.codes.eng@gmail.com" fontSize={2} color="#5024FF" /> */}
-                </Suspense>
             </Canvas>
-        </motion.div>
+        </div>
     );
 }
 
 export default Home;
-
--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -18,48 +18,69 @@ const Menu: React.FC<MenuProps> = ({ onProjectClick }) => {
                 whileHover={{ scale: 1.2 }}
             >
                 Projects
+                {isProjectsOpen && (
+                    <motion.div
+                        className={styles.projectsList}
+                        initial={{ scale: 0 }}
+                        animate={{ scale: 1 }}
+                        exit={{ scale: 0 }}
+                        transition={{ type: 'tween', duration: 0 }}
+                    >
+                        <motion.div
+                            className={styles.projectItem}
+                            onClick={onProjectClick}
+                            initial={{ scale: 1 }}
+                            whileHover={{ scale: 1.2 }}
+                        >
+                            Project 1
+                        </motion.div>
+                        <motion.div
+                            className={styles.projectItem}
+                            onClick={onProjectClick}
+                            initial={{ scale: 1 }}
+                            whileHover={{ scale: 1.2 }}
+                        >
+                            Project 2
+                        </motion.div>
+                        <motion.div
+                            className={styles.projectItem}
+                            onClick={onProjectClick}
+                            initial={{ scale: 1 }}
+                            whileHover={{ scale: 1.2 }}
+                        >
+                            Project 3
+                        </motion.div>
+                        <motion.div
+                            className={styles.projectItem}
+                            onClick={onProjectClick}
+                            initial={{ scale: 1 }}
+                            whileHover={{ scale: 1.2 }}
+                        >
+                            Project 4
+                        </motion.div>
+                    </motion.div>
+                )}
             </motion.div>
-
             <motion.div
-                className={styles.projectsList}
-                initial={{ scale: 0 }}
-                animate={{ scale: isProjectsOpen ? 1 : 0 }}
-                exit={{ scale: 0 }}
-                transition={{ type: 'tween', duration: 0 }}
+                className={styles.menuItem}
+                initial={{ scale: 1 }}
+                whileHover={{ scale: 1.2 }}
             >
-
-                <motion.div
-                    className={styles.projectItem}
-                    onClick={onProjectClick}
-                    initial={{ scale: 1 }}
-                    whileHover={{ scale: 1.2 }}
-                >
-                    Project 1
-                </motion.div>
-                <motion.div
-                    className={styles.projectItem}
-                    onClick={onProjectClick}
-                    initial={{ scale: 1 }}
-                    whileHover={{ scale: 1.2 }}
-                >
-                    Project 2
-                </motion.div>
-                <motion.div
-                    className={styles.projectItem}
-                    onClick={onProjectClick}
-                    initial={{ scale: 1 }}
-                    whileHover={{ scale: 1.2 }}
-                >
-                    Project 3
-                </motion.div>
-                <motion.div
-                    className={styles.projectItem}
-                    onClick={onProjectClick}
-                    initial={{ scale: 1 }}
-                    whileHover={{ scale: 1.2 }}
-                >
-                    Project 4
-                </motion.div>
+                About Me
+            </motion.div>
+            <motion.div
+                className={styles.menuItem}
+                initial={{ scale: 1 }}
+                whileHover={{ scale: 1.2 }}
+            >
+                Skills
+            </motion.div>
+            <motion.div
+                className={styles.menuItem}
+                initial={{ scale: 1 }}
+                whileHover={{ scale: 1.2 }}
+            >
+                Contact
             </motion.div>
         </div>
     );
